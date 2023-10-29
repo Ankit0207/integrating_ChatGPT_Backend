f0da5fb357d1c18a171fd2cdcb617ceb9cef6b38
Ankit Choudhary <ankitchoudhary0213@gmail.com>
2023-10-30 03:17:23 +0530

    update

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const OpenAI = require('openai');
+const { OpenAIApi, CreateCompletionRequest } = require('openai'); // Updated import
+
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAIApi({ key: process.env.OPENAI_API_KEY }); // Updated API initialization
 
 app.post('/generate_content', async (req, res) => {
   // Get the user's input from the request
   const { keyword, category } = req.body;
+  console.log(keyword, category);
 
   // Use GPT-3 to generate a response
   if (!keyword) {
@@ -19,19 +21,20 @@ app.post('/generate_content', async (req, res) => {
   } else if (!category) {
     return res.status(400).json({ error: "category is missing" });
   } else {
-    let prompt = `act as you are expert in ${category} telling, Generate a ${category} about ${keyword}.`;
-    const chatCompletion = await openai.chat.completions.create({
+    let prompt = `act as you are an expert in ${category} telling, Generate a ${category} about ${keyword}.`;
+    
+    const completionRequest = new CreateCompletionRequest({ // Updated request creation
       model: "gpt-3.5-turbo",
       messages: [{ role: "user", content: prompt }],
       max_tokens: 100,
     });
-    res.send({ "msg": chatCompletion.choices[0].message.content })
+
+    const chatCompletion = await openai.createCompletion(completionRequest); // Updated API method
+
+    res.send({ "msg": chatCompletion.choices[0].message.content });
   }
-
 });
-
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
-
