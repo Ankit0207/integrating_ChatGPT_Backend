2f5d217cd7bfba53b0beef343b89860c77b6e92b
Ankit Choudhary <ankitchoudhary0213@gmail.com>
2023-10-30 23:34:17 +0530

    update

--- index.js
+++ index.js
@@ -1,41 +1,38 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const OpenAI = require("openai");
 
-const apiKey = process.env.OPENAI_API_KEY;
-
-const openai = new OpenAI({ apiKey });
+const express = require('express');
+const axios = require('axios');
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(cors());
-app.use(express.static('public'));
 
 app.post('/get-response', async (req, res) => {
-  const { category, keyword } = req.body;
-
-
-  if (!keyword) {
-    return res.status(400).json({ error: "keyword is missing" });
-  } else if (!category) {
-    return res.status(400).json({ error: "category is missing" });
-  } else {
-    const response = await openai.chat.completions.create({
-      messages: [
-        { role: "system", content: `Act as you are expert in ${category} generator.` },
-        { role: "user", content: `Generate a ${category} about ${keyword}.` },
-      ],
-      model: "gpt-3.5-turbo",
+  try {
+    const { category, keyword } = req.body;
+    const response = await axios.post('https://api.openai.com/v1/engines/davinci-codex/completions', {
+      prompt: `Act as you are expert in ${category} generator, generate a ${category} about ${keyword}`,
+      max_tokens: 100,
+      temperature: 0.7,
+      n: 1
+    }, {
+      headers: {
+        'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
+        'Content-Type': 'application/json'
+      }
     });
+    console.log(response)
 
-    return res.status(200).send({ response: response.choices[0].message.content });
+    const result = response.data.choices[0].text.trim();
+    res.json({ result });
+  } catch (error) {
+    console.error('Error:', error.response.data);
+    res.status(500).json({ error: 'Something went wrong' });
   }
 });
 
-
-
 app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
+
